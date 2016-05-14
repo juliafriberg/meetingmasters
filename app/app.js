0ccff1424b7525eef824842dcedca0ba73959103
Julia Friberg <juliafriberg93@gmail.com>
2016-05-14 11:29:53 +0200

    Change screen when login is clicked

--- app/app.js
+++ app/app.js
@@ -2,15 +2,14 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.loginCtrl'
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/index'});
-}]).
-myApp.controller("loginCtrl", function($scope) {
-  $scope.loggedin = true; /*function(){}*/
-    $scope.login = function () {
-        $scope.loggedin = !$scope.loggedin;
-    }
-});
+}])
+.controller('loginCtrl', [function($scope) {
+  $scope.loggedin = true;
+  $scope.login = function () {
+    $scope.loggedin = !$scope.loggedin;
+  }
+}]);
